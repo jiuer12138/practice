cc71af8b8943aceb8ad091db4c4660ad160298e0
jiuer12138 <18815283494@163.com>
2022-06-27 20:20:34 +0800

    add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,12 @@ module.exports = {
   entry: './src/index.js', // 入口
   output: {
     path: path.resolve(__dirname, 'dist'), // 出口路径 绝对路径
-    filename: 'bundle.js', // 出口文件名
+    filename: 'main.js', // 出口文件名
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // 告诉webpack使用插件时, 以我们自己的html文件作为模板去生成dist/html文件
+      filename: 'index.html', // 生成文件的名称
+    }),
+  ],
 }
