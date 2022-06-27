9c4606791dca6cd1e64a9cf369d55a3eeac7d97b
jiuer12138 <18815283494@163.com>
2022-06-27 20:18:26 +0800

    import plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path')
-
+// 引入自动生成 html 的插件
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/index.js', // 入口
   output: {
     path: path.resolve(__dirname, 'dist'), // 出口路径 绝对路径
     filename: 'bundle.js', // 出口文件名
   },
+  plugins: [new HtmlWebpackPlugin()],
 }
-
