920535b59c4d2e476add56c0e178bc9bd3ae8ab6
jiuer12138 <18815283494@163.com>
2022-06-27 20:42:42 +0800

    configure css-loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './banner.js'
 import './tabs.js'
+import './style/index.css'
 import $ from 'jquery'
 $('#swiper').css('background-color', 'red')
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 // 引入自动生成 html 的插件
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js', // 入口
@@ -17,4 +18,17 @@ module.exports = {
     // new CleanWebpackPlugin(), // 删除的是output path 里配置的那个输出文件的文件夹
     // // 默认情况下dist
   ],
+  module: {
+    // loader 加载器 配置在这儿
+    rules: [
+      // loader的规则
+      {
+        test: /\.css$/, // 匹配所有的css文件
+        // loader 执行的顺序： use数组里从右向左运行
+        // 先用 css-loader 让webpack能够识别 css 文件的内容并打包
+        // 再用 style-loader 将样式, 把css插入到dom中
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 }
