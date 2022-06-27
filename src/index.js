1dfa6f7b3f68e76fa8ddf3dfad60ac4bdeb27e02
jiuer12138 <18815283494@163.com>
2022-06-27 21:08:25 +0800

    webpack4 url-loader

--- src/index.js
+++ src/index.js
@@ -4,3 +4,12 @@ import './style/index.css'
 // import $ from 'jquery'
 // $('#swiper').css('background-color', 'red')
 import './style/index.less'
+import imgUrl from './assets/22.png'
+let img = document.createElement('img')
+img.src = imgUrl
+document.body.appendChild(img)
+// 引入图片-使用
+import imgUrl1 from './assets/33.png'
+const theImg = document.createElement("img")
+theImg.src = imgUrl1
+document.body.appendChild(theImg)
